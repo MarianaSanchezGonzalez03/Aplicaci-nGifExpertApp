44d64f52cf9889d2ac9da2ebab588ae92c5638dc
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2022-02-25 21:26:29 -0600

    GifExpertApp - Component

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import './index.css';
+
+// Componente
+
+export const GiftExpertApp = () => {
+
+    return (
+
+      <>
+        <h2>GiftExpertApp</h2>
+        <hr />
+      </>
+
+    );
+} 
