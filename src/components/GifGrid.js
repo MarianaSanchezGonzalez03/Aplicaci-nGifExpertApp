d34e00db019919da08beba144ab031ef187dd490
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2022-02-25 23:41:53 -0600

    Mostrar los titulos de las imagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
-    const [count, setCount] = useState(0)
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
         getGifs();
@@ -21,6 +22,7 @@ export const GifGrid = ({category}) => {
         })
 
         console.log(gifs);
+        setImages( gifs );
 
 
     }
@@ -30,8 +32,16 @@ export const GifGrid = ({category}) => {
   return (
     <div>
         <h3>{ category }</h3>
-        <h3> {count} </h3>
-        <button onClick={ () => setCount( count + 1 ) }></button>
+        {
+            images.map( img => (
+                <GifGridItem 
+                    key = { img.id }
+                    {...img } 
+
+                />
+            ))
+        }
+
     </div>
   )
 }
