f392bde6c8f29b6ac52ded4f22b01c3228340cc0
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2022-02-26 00:57:04 -0600

    Fin

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -6,8 +6,8 @@ export const GifGridItem = ( { title, url} ) => {
 
 
   return (
-    <div className="card">
-        <img src={ url } alt={ title } />
+<div className="card animate__animated animate__backInLeft">    
+    <img src={ url } alt={ title } />
         <p> { title} </p>
     </div>
   )
