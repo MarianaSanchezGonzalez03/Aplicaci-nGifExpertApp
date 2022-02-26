21b34b462511fa583074f3710bf9dccdc9e5b333
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2022-02-25 21:47:35 -0600

    Creando una lista de categorias

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import './index.css';
@@ -6,13 +6,27 @@ import './index.css';
 // Componente
 
 export const GiftExpertApp = () => {
+    //const categories = ['Demon Slayer', 'One punch', 'Attack on Titan'];
+    const [categories, setCategories] = useState(['Demon Slayer', 'One punch', 'Attack on Titan']);
+
+    const handleAdd = () => {
+        setCategories( [...categories, 'Full Metal Alchemist'] );
+    }
 
     return (
 
       <>
         <h2>GiftExpertApp</h2>
         <hr />
-      </>
+        <button onClick = {handleAdd}>Agregar</button>
 
-    );
-} 
+        <ol>
+            {
+                categories.map(category => {
+                    return <li  key = { category }>{ category }</li>
+                })
+            }
+        </ol>
+      </>
+)
+}
